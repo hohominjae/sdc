06eb1863b5210d7ed5bcdc27c3b8e200251a577c
hohominjae <jmj0531@naver.com>
2023-07-18 10:31:08 +0900

    리뷰 엔티티 구성..?

--- src/main/java/com/sparta/sdc/review/entity/Review.java
+++ src/main/java/com/sparta/sdc/review/entity/Review.java
@@ -1,4 +1,39 @@
 package com.sparta.sdc.review.entity;
 
+import com.sparta.sdc.order.entity.Order;
+import com.sparta.sdc.shop.entity.Shop;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "review")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+    @OneToMany(mappedBy = "Menu")
+    private Menu menu;
+
+    @OneToOne
+    private Order order;
+
+
 }
