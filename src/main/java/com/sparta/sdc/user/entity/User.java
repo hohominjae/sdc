e302228a10669c85fd7bb1f7c34bc32751e07400
choi <tls9607@naver.com>
2023-07-18 11:07:58 +0900

    User, UserRoleEnum, JwtUtil, UserDetailsImpl 구현

--- src/main/java/com/sparta/sdc/user/entity/User.java
+++ src/main/java/com/sparta/sdc/user/entity/User.java
@@ -1,4 +1,33 @@
 package com.sparta.sdc.user.entity;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+@EqualsAndHashCode
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false) //필드 추가 , EnumType.ORDINAL는 integer타입(숫자, 순번)으로 db에 저장하는거. ORDINAL로 저장할 시 다른 객체(?)가 들어오면 db에 있는 데이터가 꼬이기 때문
+    @Enumerated(value = EnumType.STRING) //이넘 클래스를 엔티티의 컬럼으로 사용하기 위한 어노테이션. 이 값을 어떤 형태로 db에 저장할지 정의.
+    private UserRoleEnum role;
+
+    public User(String username, String password,  UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
--- src/main/java/com/sparta/sdc/user/service/UserService.java
+++ src/main/java/com/sparta/sdc/user/service/UserService.java
@@ -1,4 +1,41 @@
 package com.sparta.sdc.user.service;
 
+import com.sparta.sdc.user.dto.AuthRequestDto;
+import com.sparta.sdc.user.entity.User;
+import com.sparta.sdc.user.entity.UserRoleEnum;
+import com.sparta.sdc.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public final UserRepository userRepository;
+    public void signup(AuthRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        UserRoleEnum role = requestDto.getRole();
+
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+        User user = new User(username, password, role);
+        userRepository.save(user);
+    }
+
+    public void login(AuthRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+        //사용자 확인(useername이 없는 경우)
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException("등록된 사용자가 없습니다."));
+
+        //비밀번호 확인(password가 다른 경우)
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
 }
