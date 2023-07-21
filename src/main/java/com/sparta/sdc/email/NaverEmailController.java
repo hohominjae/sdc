2fbb6957edb487e51a5264c94ce206ab69fac8d1
choi <tls9607@naver.com>
2023-07-21 17:12:36 +0900

    네이버 이메일 인증 구현 - 오류발생으로 보류.

--- src/main/java/com/sparta/sdc/email/NaverEmailController.java
+++ src/main/java/com/sparta/sdc/email/NaverEmailController.java
@@ -1,24 +1,23 @@
-//package com.sparta.sdc.email;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/api/sdc")
-//public class NaverEmailController {
-//    private final NaverEmailService naverEmailService;
-//
-//    @PostMapping("signup/mailConfirm")
-//    @ResponseBody
-//    public String mailConfirm(@RequestParam String email) throws Exception {
-//        log.info("이메일 " + email );
-//
-//        String code = naverEmailService.sendSimpleMessage(email);
-//        log.info("인증코드 : " + code);
-//
-//        return code;
-//    }
-//}
+package com.sparta.sdc.email;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sdc")
+public class NaverEmailController {
+    private final NaverEmailService naverEmailService;
+
+    @PostMapping("signup/mailConfirm")
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        log.info("이메일 " + email );
+
+        String code = naverEmailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+
+        return code;
+    }
+}
